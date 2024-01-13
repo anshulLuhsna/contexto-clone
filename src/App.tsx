e5fc943c5e47c45a2f8720b120a92422274a1447
anshulLuhsna <anshulkalbande@gmail.com>
2024-01-14 01:43:37 +0530

    Update 1.1.1

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home';  
-import About from './components/game';  
+import Game from './components/game';  
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/game" element={<Game />} />
       </Routes>
     </Router>
   );
