3a3809de35eb9e2df3266e43252d3a60300dd904
anshulLuhsna <138516850+anshulLuhsna@users.noreply.github.com>
2024-01-14 01:15:59 +0530

    fix: code cleanup

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Home: React.FC = () => {
   return (
     <div className='w-full flex flex-col items-center'>
-      <h1 className='text-center mt-8'>CONTEXTO FOR MUMBO</h1>
+      <h1 className='text-center mt-8'>CONTEXTO</h1>
       <Link to="/about" className='mt-8 text-4xl'>PLAY</Link>
     </div>
   );
